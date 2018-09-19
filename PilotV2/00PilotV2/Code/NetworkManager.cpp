efc61268bbc56f8afab8c98e904da4ebad35f98c
Michael Barnett <michaelbrettbarnett@gmail.com>
2018-09-19 16:30:14 +1000

    Readded network manager after the deletion problem

---
+++ PilotV2/00PilotV2/Code/NetworkManager.cpp
@@ -0,0 +1,17 @@
+#include "NetworkManager.h"
+
+NetworkManager::NetworkManager() {
+
+}
+
+NetworkManager::~NetworkManager() {
+
+}
+
+void NetworkManager::sendToClient(/* To determine parameters later */) {
+	
+}
+
+void NetworkManager::recvFromClient(/* To determine parameters later */) {
+
+}
---
+++ PilotV2/00PilotV2/Code/NetworkManager.h
@@ -0,0 +1,16 @@
+#include <thread>
+#include <vector>
+
+class NetworkManager {
+public:
+	NetworkManager();
+	~NetworkManager();
+
+	void sendToClient(/* To determine parameters later */);
+	void recvFromClient(/* To determine parameters later */);
+
+	// ShipNetData contain data to send over the network. ShipSendData allows us to grab an array of bytes of said data.
+	inline struct ShipNetData { int posx, posy, direction; /* to discuss what we exactly need */ };
+	inline union ShipSendData { ShipNetData s_data; char* buffer[sizeof(ShipNetData)]; };
+
+};
